b51db412dc8a152a36f8a9a76872f2f2758d6daa
NANAQS <otaviotokyo5@gmail.com>
2024-10-11 18:52:16 -0300

    chore: add basic project

---
+++ src/main/kotlin/com/example/nanak_microsservices/NanakMicrosservicesApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nanak_microsservices
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NanakMicrosservicesApplication
+
+fun main(args: Array<String>) {
+	runApplication<NanakMicrosservicesApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nanak_microsservices/NanakMicrosservicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nanak_microsservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NanakMicrosservicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
